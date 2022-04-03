d23d763d9065c3883fc2d98fb9962b9136d960df
Pierce <contact@mokou.io>
2022-04-03 14:59:12 -0400

    Update overlay-userscript.js

--- overlay-userscript.js
+++ overlay-userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Touhou r/Place Hijack 2022 overlay.
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  try to take over the canvas! modified from r/OsuPlace's script
 // @author       oralekin, LittleEndu, ekgame, Mokou
 // @match        https://hot-potato.reddit.com/embed*
