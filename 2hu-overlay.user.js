744a4133c6cff1f0f551d604ca0306b2c26eb846
BenMan95 <81588289+BenMan95@users.noreply.github.com>
2022-04-04 02:43:47 -0500

    Split overlay files (#3)
    * added split overlay files
    
    Modifying the current 6000x6000 px overlay file is cumbersome. This splits it into separate files for each area, along with a txt file to record each image location and coordinates
    
    * Modified script to use multiple overlay files
    
    This modifies the script to use the split overlay files described in the previous commit
    
    * changed image file prefixes
    
    * changed text file url

--- 2hu-overlay.user.js
+++ 2hu-overlay.user.js
@@ -1,29 +1,47 @@
 // ==UserScript==
 // @name         [AUTO UPDATING] Touhou r/Place Hijack 2022 overlay
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the canvas! modified from r/OsuPlace's script
 // @author       oralekin, LittleEndu, ekgame, Mokou
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @downloadURL  https://2hu-rplace.mokou.io/2hu-overlay.user.js
 // @updateURL    https://2hu-rplace.mokou.io/2hu-overlay.user.js
-// @grant        none
+// @connect      2hu-rplace.mokou.io
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-        // Load the image
-        const image = document.createElement("img");
-        image.src = "https://2hu-rplace.mokou.io/overlays/2hu-combined.png";
-        image.onload = () => {
-            image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
-        };
-      
-        // Add the image as overlay
         const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
         const canvas = camera.querySelector("mona-lisa-canvas");
-        canvas.shadowRoot.querySelector('.container').appendChild(image);
-      
+
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: "https://2hu-rplace.mokou.io/overlays/overlays.txt",
+            onload: function(r) {
+                const lines = r.responseText.split('\n');
+                for (let line of lines) {
+                    if (!line) continue;
+                    const vals = line.split(' ');
+                    const x = parseInt(vals[0]);
+                    const y = parseInt(vals[1]);
+                    const url = vals[2];
+
+                    // Load the image
+                    const image = document.createElement("img");
+                    image.src = url;
+                    image.onload = () => {
+                        image.style = `position: absolute; left: ${x}px; top: ${y}px; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
+                        console.log(image.style);
+                    };
+
+                    // Add the image as overlay
+                    canvas.shadowRoot.querySelector('.container').appendChild(image);
+                }
+            }
+        });
+
         // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
             const preview = camera.querySelector("mona-lisa-pixel-preview");
